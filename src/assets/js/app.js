3f7fd62380b46e989bd4e4ee18e26a64376fb755
PrzemekMiros <kontakt@futurewebstudio.pl>
2022-12-26 19:46:31 +0100

    Add scroll top

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,33 +3,11 @@
 
 // Gsap config
 
-  gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-  
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector(".smooth-scroll"),
-    smooth: true,
-    multiplier: 0.75
-  });
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-  locoScroll.on("scroll", ScrollTrigger.update);
-  
-  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy(".smooth-scroll", {
-    scrollTop(value) {
-      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-      return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
-  });
-  
+function gsapAnimation() {
 
-
-  const triggers = document.querySelectorAll(".word");
+  const triggers = document.querySelectorAll(".split-word .word");
   
   triggers.forEach(trigger => {
     gsap.from(trigger.children, {
@@ -46,16 +24,21 @@
   })
   
 
-    gsap.from(".split-line", {
-      duration: 1.5,
-      y: "110%",
-      ease: Power4. easeOut,
-      stagger: 0.15,
-      scrollTrigger: {
-        trigger: ".split-line",
-        scroller: ".smooth-scroll",
-        start: "top 85%",
-      }
+
+    const target = document.querySelectorAll('.split-line');
+    const results = Splitting({ target: target, by: 'lines' });
+  
+    results[0].lines.forEach((line, index) => {
+      line.forEach((word) => {
+        gsap.from(word, { 
+          y: "100%", 
+          stagger: .1,
+        scrollTrigger: {
+          trigger: target,
+          scroller: ".smooth-scroll",
+          start: "top 85%",
+            }});
+      })
     });
 
   
@@ -78,6 +61,23 @@
     hoverLink.addEventListener("mouseleave", (e) => tl.reverse());
 
 
+/*
+    const hoverLinkPorfolio = document.querySelector('.site-link-portfolio');
+    
+    const tl2 = gsap.timeline({ 
+      defaults: { ease: "power4.easeOut" }, 
+      paused: true 
+    })
+      .to(arrow, { 
+        x: "40%",
+
+      })
+    
+    
+      hoverLinkPorfolio.addEventListener("mouseenter", (e) => tl2.play());
+      hoverLinkPorfolio.addEventListener("mouseleave", (e) => tl2.reverse());
+*/
+
   // Progress
     gsap.to('progress', {
       value: 100,
@@ -96,6 +96,9 @@
   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
   
+}
+
+gsapAnimation();
 
 /*
 // Swup config
@@ -120,5 +123,4 @@ document.addEventListener("swup:animationInDone", () => {
   
 }), console.clear();
 */
-
 
