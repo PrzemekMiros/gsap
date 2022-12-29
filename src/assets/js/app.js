7db79baf8500cf523bc3a005c77cfc5625a5a42d
PrzemekMiros <kontakt@futurewebstudio.pl>
2022-12-29 21:13:54 +0100

    Add multiple animation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,69 +1,80 @@
 
-
-
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-  
-    const locoScroll = new LocomotiveScroll({
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  const locoScroll = new LocomotiveScroll({
         el: document.querySelector(".smooth-scroll"),
         smooth: true,
         multiplier: 0.75
       });
-      // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-      locoScroll.on("scroll", ScrollTrigger.update);
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
       
-      // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-      ScrollTrigger.scrollerProxy(".smooth-scroll", {
-        scrollTop(value) {
-          return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-        getBoundingClientRect() {
-          return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-        },
-      });
-
+  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".smooth-scroll", {
+      scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+    });
 
   gsap.registerPlugin(ScrollTrigger);
 
-  const triggers = document.querySelectorAll(".split-word .word");
-  
-  triggers.forEach(trigger => {
-    gsap.from(trigger.children, {
+  // Reveal split text chars
+  const reveals = gsap.utils.toArray(".char .reveal");
+  reveals.forEach(reveal => {
+    gsap.from(reveal, {
       duration: 1,
-      y: "110%",
-      ease: Power4. easeOut,
+      x: "-100%",
+      ease: Power1. easeOut,
       stagger: 0.1,
       scrollTrigger: {
-        trigger: trigger,
+        trigger: reveal,
         scroller: ".smooth-scroll",
-        start: "top 85%",
+        start: "top 80%",
       }
     });
-  })
-  
+  });
 
-    const target = document.querySelectorAll('.split-line');
-    const results = Splitting({ target: target, by: 'lines' });
-  
-    results[0].lines.forEach((line, index) => {
-      line.forEach((word) => {
-        gsap.from(word, { 
-          y: "100%", 
-          stagger: .1,
-        scrollTrigger: {
-          trigger: target,
-          scroller: ".smooth-scroll",
-          start: "top 85%",
-            }});
-      })
+  // Reveal split text lines
+  const revealSplit = gsap.utils.toArray(".split-line");
+  revealSplit.forEach(revsplit => {
+    gsap.from(revsplit, {
+      duration: .8,
+      y: "100%",
+      ease: Power1. easeOut,
+      stagger: 0.1,
+      scrollTrigger: {
+        trigger: revsplit,
+        scroller: ".smooth-scroll",
+        start: "top 90%",
+      }
     });
+  });
 
-  
+  // Fade from down
+  const boxes = gsap.utils.toArray('.fade-down');
+  boxes.forEach(box => {
+  gsap.from(box, { 
+    duration: .8,
+    y: "100%",
+    opacity: 0,
+    delay: .2,
+    ease: Power1. easeOut,
+    scrollTrigger: {
+      trigger: box,
+      scroller: ".smooth-scroll",
+      start: "top 95%",
+    }
+  })
+});
 
+  // Hover link
   const hoverLink = document.querySelector('.contact-link');
   const arrow = document.querySelector('.arrow');
   
   const tl = gsap.timeline({ 
-    defaults: { ease: "power4.easeOut" }, 
+    defaults: { ease: "power1.easeOut" }, 
     paused: true 
   })
     .to(arrow, { 
@@ -74,8 +85,6 @@
   
     hoverLink.addEventListener("mouseenter", (e) => tl.play());
     hoverLink.addEventListener("mouseleave", (e) => tl.reverse());
-
-
 
   // Progress
     gsap.from('.progress', {
@@ -89,8 +98,6 @@
       }
     });
 
-
-    
   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
   
