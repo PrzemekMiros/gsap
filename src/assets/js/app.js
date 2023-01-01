76ba9434396c84c259e42b458ceb8d19d5b7e92a
PrzemekMiros <kontakt@futurewebstudio.pl>
2023-01-01 23:22:04 +0100

    Fixes 2

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,27 +1,23 @@
 
 function portfolioList() {
 
-    const portfolioLink1 = document.querySelector(".p-link-1");
-    const portfolioImage1 = document.querySelector(".p-img-1");
-  
-  
-    portfolioLink1.addEventListener("mouseenter", () => {
-      portfolioLink2.classList.remove("active")
-      portfolioImage2.classList.remove("active")
-      portfolioLink1.classList.add("active")
-      portfolioImage1.classList.add("active")
-  })
-  
-    const portfolioLink2 = document.querySelector(".p-link-2");
-    const portfolioImage2 = document.querySelector(".p-img-2");
-  
-  
-    portfolioLink2.addEventListener("mouseenter", () => {
-      portfolioLink1.classList.remove("active")
-      portfolioImage1.classList.remove("active")
-      portfolioLink2.classList.add("active")
-      portfolioImage2.classList.add("active")
-  })
+  const portfolioLink = document.querySelectorAll(".portfolio-link"),
+        portfolioLink1 = document.querySelector(".p-link-1"),
+
+        portfolioImg1 = document.querySelector(".p-img-1");
+
+  portfolioLink.forEach(function (item) {
+  item.addEventListener(
+    "mouseenter",
+    function (e) {
+      // ADDS AND REMOVES ACTIVE CLASS ON TABLINKS
+      portfolioLink.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+    },
+  );
+});
 }
 
 portfolioList();
