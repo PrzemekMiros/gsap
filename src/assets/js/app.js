16686028fbce2f2c9acd8d28a6ecb80b6b24cf84
PrzemekMiros <kontakt@futurewebstudio.pl>
2023-01-02 21:09:31 +0100

    Add animation to portfolio

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,8 +3,17 @@ function portfolioList() {
 
   const portfolioLink = document.querySelectorAll(".portfolio-link"),
         portfolioLink1 = document.querySelector(".p-link-1"),
+        portfolioLink2 = document.querySelector(".p-link-2"),
+        portfolioLink3 = document.querySelector(".p-link-3"),
+        portfolioLink4 = document.querySelector(".p-link-4"),
+        portfolioLink5 = document.querySelector(".p-link-5"),
 
-        portfolioImg1 = document.querySelector(".p-img-1");
+        portfolioImg = document.querySelectorAll(".portfolio-img"),
+        portfolioImg1 = document.querySelectorAll(".p-img-1"),
+        portfolioImg2 = document.querySelectorAll(".p-img-2"),
+        portfolioImg3 = document.querySelectorAll(".p-img-3"),
+        portfolioImg4 = document.querySelectorAll(".p-img-4"),
+        portfolioImg5 = document.querySelectorAll(".p-img-5");
 
   portfolioLink.forEach(function (item) {
   item.addEventListener(
@@ -18,8 +27,33 @@ function portfolioList() {
     },
   );
 });
-}
 
+portfolioLink1.addEventListener("mouseenter", () => {
+  portfolioImg.forEach(el=>el.classList.remove('active'));
+  portfolioImg1.forEach(el=>el.classList.add('active'));
+});
+
+portfolioLink2.addEventListener("mouseenter", () => {
+  portfolioImg.forEach(el=>el.classList.remove('active'));
+  portfolioImg2.forEach(el=>el.classList.add('active'));
+});
+
+portfolioLink3.addEventListener("mouseenter", () => {
+  portfolioImg.forEach(el=>el.classList.remove('active'));
+  portfolioImg3.forEach(el=>el.classList.add('active'));
+});
+
+portfolioLink4.addEventListener("mouseenter", () => {
+  portfolioImg.forEach(el=>el.classList.remove('active'));
+  portfolioImg4.forEach(el=>el.classList.add('active'));
+});
+
+portfolioLink5.addEventListener("mouseenter", () => {
+  portfolioImg.forEach(el=>el.classList.remove('active'));
+  portfolioImg5.forEach(el=>el.classList.add('active'));
+});
+
+}
 portfolioList();
 
 
@@ -34,7 +68,6 @@ function greetingsText () {
     
     greeting.innerHTML = welcomeText;
     }
-    
   greetingsText();
 
 
@@ -49,5 +82,16 @@ function greetingsText () {
       }
     })
   }
-  
   accordionWrap();  
+
+  
+  const contactLink = document.querySelector(".contact-link");
+  const contactArrow = document.querySelector(".arrow");
+
+  contactLink.addEventListener("mouseenter", () => {
+    contactArrow.classList.add("active");
+  });
+
+  contactLink.addEventListener("mouseleave", () => {
+    contactArrow.classList.remove("active");
+  })
